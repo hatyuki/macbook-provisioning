3de959dac17fd31799d3c0caadf050155b856df0
hatyuki <hatyuki29+git@gmail.com>
2026-03-12 10:42:00 +0900

    feat(config/git): generate gitconfig from template
    Git の設定ファイルを ERB から生成するように変更した
    
    - `git/config` を `git/config.erb` にリネームし、ユーザー設定 (`[user]`) を分離
    - 環境 (`node[:name]`) に応じて `includes/user.<name>` を include するように変更
    - `op` で設定ファイルを生成できるように対応
    - `nodes/*.yaml` に環境名を識別するための `name` 属性を追加

--- mitamae/recipes/dotfiles.rb
+++ mitamae/recipes/dotfiles.rb
@@ -1,5 +1,9 @@
+include_recipe "../definitions/op"
 include_recipe "../definitions/stow"
 
+dotfiles = "#{ROOT_DIR}/dotfiles"
+
+# stow
 [
   :config,
   :ideavim,
@@ -7,7 +11,19 @@ include_recipe "../definitions/stow"
   :zsh,
 ].each do |pkg|
   stow pkg do
-    dir File.expand_path("../../../dotfiles", __FILE__)
+    dir dotfiles
     use_nix_shell true
+  end
+end
+
+# Git
+template "#{dotfiles}/config/.config/git/config" do
+  source "#{dotfiles}/config/.config/git/config.erb"
+  variables node: node[:name]
+end
+
+if node[:name] == "work"
+  op "#{dotfiles}/config/.config/git/includes/user.work" do
+    source "#{dotfiles}/config/.config/git/includes/user.work.tpl"
   end
 end

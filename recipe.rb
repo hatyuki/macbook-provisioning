d31f2200cb4097c2ce668a2969ad72c949123f82
hatyuki <hatyuki29+git@gmail.com>
2026-03-12 10:42:00 +0900

    feat(config/git): generate gitconfig from template
    Git の設定ファイルを ERB から生成するように変更した
    
    - `git/config` を `git/config.erb` にリネームし、ユーザー設定 (`[user]`) を分離
    - 環境 (`node[:name]`) に応じて `includes/user.<name>` を include するように変更
    - `op` で設定ファイルを生成できるように対応
    - `nodes/*.yaml` に環境名を識別するための `name` 属性を追加

--- recipe.rb
+++ recipe.rb
@@ -1,10 +1,12 @@
-# SEE ALSO: https://nix.dev/install-nix
-execute :install_nix do
-  # TODO: Linux では Option が必要
-  command "curl -L https://nixos.org/nix/install | sh"
-  not_if "command -v nix"
-end
+ROOT_DIR = File.dirname(__FILE__)
 
+# SEE ALSO: https://nix.dev/install-nix
+# execute :install_nix do
+#   # TODO: Linux では Option が必要
+#   command "curl -L https://nixos.org/nix/install | sh"
+#   not_if "command -v nix"
+# end
+#
 execute "nix run nix-darwin" do
   command "sudo nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --flake #{node[:flake]}"
 end
